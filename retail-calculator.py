186d46c4d891c23cf19b56a4fb675af556acc1ef
cjcoronado <95267020+cjcoronado@users.noreply.github.com>
2022-12-08 18:05:28 -0500

    Create retail-calculator.py
    This python script allows the user to enter how many cassettes they are manufacturing, the costs of manufacturing, set the profit margin they desire, and print the retail price for said margin.

---
+++ retail-calculator.py
@@ -0,0 +1,9 @@
+number_of_tapes = f'Enter the number of cassette tapes being manufactured: '
+cost_of_tapes = f'Enter the manufacturing costs for the cassette tapes: '
+unit_cost = cost_of_tapes / number_of_tapes
+print(f'Unit cost of tapes is ${unit_cost}.')
+print()
+
+profit_margin = f'How much of a profit margin do you want per item? Enter as a decimal (ex: .5 for 50%, 1.0 for 100%): '
+tape_retail = unit_cost * (1 + profit_margin)
+print(f'The retail price for the tapes are ${tape_retail}')
